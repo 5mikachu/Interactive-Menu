1d88339f3f7a990f33644bfd7ae6703d775afded
micah <micah.vanderhorst@icloud.com>
2025-02-16 14:01:40 +0100

    htmx

--- app.py
+++ app.py
@@ -1,107 +1,102 @@
 import json
 import logging
-
 from flask import Flask, request, render_template
-
-
-def load_menu_items() -> dict:
-    """
-    Load menu items from menuitems.json
-
-    :return:
-    """
-    menu_items_files: str = 'menuitems.json'
-    try:
-        with open(menu_items_files, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        logging.warning("Error loading menuitems.json")
-        return {}
-
-
-def load_allergies() -> dict:
-    """
-    Load allergies from allergies.json
-
-    :return:
-    """
-    allergies_file: str = 'allergies.json'
-    try:
-        with open(allergies_file, 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        logging.warning("Error loading allergies.json")
-        return {}
-
-
-def get_formatted_items(menu_items: dict) -> list:
-    """
-    Format menu items for display
-
-    :param menu_items:
-    :return:
-    """
-    sorted_menu_items: list[tuple[str]] = sorted(menu_items.items())
-    return [f"{name}: {', '.join(ingredients)}" for name, ingredients in sorted_menu_items]
-
-
-def list_items_by_allergen_status(selected_allergies: list) -> tuple[list, list]:
-    """
-    List menu items based on whether they contain specified allergens
-
-    :param selected_allergies:
-    :return:
-    """
-    menu_items: dict = load_menu_items()
-    allergy_data: dict = load_allergies()
-    can_eat: list = []
-    cannot_eat: list = []
-    allergen_ingredients: list = []
-
-    for allergy in selected_allergies:
-        if allergy in allergy_data:
-            allergen_ingredients.extend(allergy_data[allergy])
-
-    for menu_item, menu_item_ingredients in menu_items.items():
-        if any(ingredient in allergen_ingredients for ingredient in menu_item_ingredients):
-            cannot_eat.append(menu_item)
-        else:
-            can_eat.append(menu_item)
-
-    # Add "Baked Air" if no items remain after filtering for allergens
-    if not can_eat:
-        can_eat.append("Baked Air")
-
-    return can_eat, cannot_eat
 
 
 app = Flask(__name__)
 
 
+def load_json(file_name: str) -> dict:
+    """
+    Load a JSON file and return its contents as a dictionary
+
+    :param file_name:
+    :return:
+    """
+    try:
+        with open(file_name, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        logging.warning(f"Error loading {file_name}")
+        return {}
+
+
+def get_formatted_items(menu_items: dict) -> list[dict]:
+    """
+    Format menu items as a list of dictionaries
+
+    :param menu_items:
+    :return:
+    """
+    return [{"name": name, "ingredients": ', '.join(details['ingredients']), "price": details['price']} for name, details in sorted(menu_items.items())]
+
+
+def list_items_by_allergen_status(selected_allergies) -> tuple[list[str], list[str]]:
+    """
+    List menu items by allergen status
+
+    :param selected_allergies:
+    :return:
+    """
+    menu_items: dict = load_json('menuitems.json')
+    allergies: dict = load_json('allergies.json')
+    can_eat, cannot_eat = [], []
+    allergen_ingredients: list = [ingredient for allergy in selected_allergies if allergy in allergies for ingredient in allergies[allergy]]
+
+    for name, details in menu_items.items():
+        ingredients = details['ingredients']
+        (cannot_eat if any(ingredient in allergen_ingredients for ingredient in ingredients) else can_eat).append(name)
+
+    return can_eat, cannot_eat
+
+
+def filter_items_by_course(menu_items, selected_course) -> dict:
+    """
+    Filter menu items by course
+
+    :param menu_items:
+    :param selected_course:
+    :return:
+    """
+    if selected_course:
+        return {name: details for name, details in menu_items.items() if details['course'] == selected_course}
+    return menu_items
+
+
+def filter_items(menu_items: dict, selected_allergies: list[str], selected_course: str) -> dict:
+    """
+    Filter menu items by all selected criteria
+
+    :param menu_items:
+    :param selected_allergies:
+    :param selected_course:
+    :return:
+    """
+    can_eat, _ = list_items_by_allergen_status(selected_allergies)
+
+    # Filter by course
+    filtered_items = filter_items_by_course(menu_items, selected_course)
+    can_eat = {name: details for name, details in filtered_items.items() if name in can_eat}
+
+    return can_eat
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    allergies: list = list(load_allergies().keys())
-    menu_items: dict = load_menu_items()
-    filtered_menu: list = []
+    allergies: list[str] = list(load_json('allergies.json').keys())
+    menu_items: dict = load_json('menuitems.json')
+    courses: list[str] = sorted(set(details['course'] for details in menu_items.values()))
 
     if request.method == 'POST':
-        selected_allergies = request.form.getlist('allergies')
-        
-        if not selected_allergies:
-            selected_allergies: list = []
+        selected_allergies: list[str] = request.form.getlist('allergies') or []
+        selected_course: str = request.form.get('course') or ''
+        filtered_menu_items: dict = filter_items(menu_items, selected_allergies, selected_course)
+        filtered_menu: list = get_formatted_items(filtered_menu_items)
+        return render_template('menu_items.html', menu=filtered_menu)
 
-        can_eat, _ = list_items_by_allergen_status(selected_allergies)
-
-        for name in can_eat:
-            ingredients = menu_items[name]
-            filtered_menu.append({"name": name, "ingredients": ', '.join(ingredients)})
-    else:
-        formatted_menu_items = get_formatted_items(menu_items)
-        for formatted_item in formatted_menu_items:
-            name, ingredients = formatted_item.split(":")
-            filtered_menu.append({"name": name.strip(), "ingredients": ingredients.strip()})
-
-    return render_template('index.html', allergies=allergies, menu=filtered_menu)
+    # Render all menu items on initial load
+    initial_menu: list = get_formatted_items(menu_items)
+    return render_template('index.html', allergies=allergies, courses=courses, menu=initial_menu)
 
 
 if __name__ == '__main__':
